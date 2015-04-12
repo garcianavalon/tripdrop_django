2dec9fabaa94bf7af5f6d4f0f37e2b6c96bf12f5
garcianavalon <garcianavalon@gmail.com>
2015-04-12 21:23:39 +0200

    no select2 for now

--- needs/forms.py
+++ needs/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 
-from django_select2 import fields as select2_fields
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Field, ButtonHolder, Submit
 from django_countries import countries
@@ -9,9 +8,6 @@ import needs.models as need_models
 
 # ModelForms
 class NeedForm(forms.ModelForm):
-    city = select2_fields.ModelSelect2Field(queryset=need_models.City.objects)
-    #region = select2_fields.ModelSelect2Field(queryset=need_models.Region.objects)
-    #country = select2_fields.Select2ChoiceField(choices=countries)
 
     def __init__(self, *args, **kwargs):
         super(NeedForm, self).__init__(*args, **kwargs)
@@ -22,4 +18,4 @@ class NeedForm(forms.ModelForm):
         model = need_models.Need
         # TODO(garcianavalon) this is ok for prototyping but should set explicit
         # fields for production to avoid security vulnerabilities
-        fields = []
+        fields = '__all__'
--- tripdrop/settings.py
+++ tripdrop/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django_countries',
     'crispy_forms',
-    'django_select2',
     'needs',
 )
 
