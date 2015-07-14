f6ba73ea99bde3cb0249ddb9ab3ac8723bd86700
Enrique Garcia Navalon <garcianavalon@gmail.com>
2015-07-14 12:43:31 +0200

    Added needs models to admin

--- needs/admin.py
+++ needs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from needs import models
 # Register your models here.
+
+admin.site.register(models.ContactPerson)
+admin.site.register(models.Country)
+admin.site.register(models.Region)
+admin.site.register(models.City)
+admin.site.register(models.Need)
+admin.site.register(models.Institution)
