4429764cd69d541378fd3c626bba3069e6581277
Enrique Garcia Navalon <garcianavalon@gmail.com>
2015-10-20 16:33:10 +0200

    namepsaced map functions

--- maps/static/maps/js/init_map.js
+++ maps/static/maps/js/init_map.js
@@ -1,26 +1,45 @@
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 2,
-    center: {lat: -34.397, lng: 150.644}
-  });
-  var geocoder = new google.maps.Geocoder();
+var tripdrop_map = {
 
-  document.getElementById('submit').addEventListener('click', function() {
-    geocodeAddress(geocoder, map);
-  });
-}
+  geolocated_marker: undefined,
 
-function geocodeAddress(geocoder, resultsMap) {
-  var address = document.getElementById('address').value;
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === google.maps.GeocoderStatus.OK) {
-      resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
+  init: function() {
+
+    //create variables
+    // NOTE(garcianavalon) don't use jQuery to find the map canvas!!
+    tripdrop_map.map = new google.maps.Map(document.getElementById('map'), { 
+      zoom: 2,
+      center: {lat: 4.397, lng: 15.644}
+    });
+
+    tripdrop_map.geocoder = new google.maps.Geocoder();
+    // bind events
+    $('#submit').on('click', function() {
+      tripdrop_map.geocode_address();
+    });
+
+  },
+  geocode_address: function() {
+    var address = $('#address').val();
+    tripdrop_map.geocoder.geocode({'address': address}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        // center map
+        tripdrop_map.map.setCenter(results[0].geometry.location);
+
+        // delete old marker
+        if (tripdrop_map.geolocated_marker) {
+          tripdrop_map.geolocated_marker.setMap(null);
+        }
+
+        // create new marker
+        tripdrop_map.geolocated_marker = new google.maps.Marker({
+          map: tripdrop_map.map,
+          position: results[0].geometry.location
+        });
+
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+};
